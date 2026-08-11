7d0baf625787cdc0db6dd720078e1dc1074f5578
Alex Dickson <alex.dickson@immfly.com>
2026-08-11 18:14:31 +0200

    Add new CORS origin

--- src/plugins/cors.ts
+++ src/plugins/cors.ts
@@ -4,7 +4,10 @@ import fp from "fastify-plugin"
 export default fp(
   async (fastify) => {
     await fastify.register(cors, {
-      origin: process.env.CORS_ORIGIN ?? "http://localhost:5173",
+      origin: [
+        process.env.CORS_ORIGIN ?? "http://localhost:5173",
+        "https://slopifyproject.com",
+      ],
     })
   },
   { name: "cors" },
