d35db4776e57b060445ba4dbd90974e3808bd645
alexdicksonroig <alexdicksonroig@gmail.com>
2026-08-13 18:50:12 +0200

    Delete migrations

--- drizzle/0001_grey_skin.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "products" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"name" text NOT NULL,
-	"description" text,
-	"price_in_cents" integer NOT NULL,
-	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
-	"updated_at" timestamp with time zone DEFAULT now() NOT NULL
-);
--- drizzle/0003_wealthy_stranger.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE "variants" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"possible_options" text[] NOT NULL,
-	"label" text NOT NULL
-);
